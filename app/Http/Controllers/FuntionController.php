72bda71e1944c5d098a0bfbcca4eb46409435cc3
Skgrendel <bryan.andion@gmail.com>
2024-07-04 11:19:41 -0500

    cambios

--- app/Http/Controllers/FuntionController.php
+++ app/Http/Controllers/FuntionController.php
@@ -24,11 +24,9 @@ class FuntionController extends Controller
     public function BuscarContrato($id)
     {
         $gis = $this->info->DataGisubicacion($id);
-        dd($gis);
 
         if ($gis) {
             $src =   $gis['geometry']['latitude']   . ',' .  $gis['geometry']['longitude'];
-
             return response()->json(['src' => $src, 'contrato' => $gis['info']]); // Si el contrato existe, devuelve sus datos como JSON
         } else {
             return response()->json(['error' => 'Contrato no encontrado'], 404); // Si no existe, devuelve un error
