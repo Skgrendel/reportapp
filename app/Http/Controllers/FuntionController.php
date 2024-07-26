337c0568f7f01c57646d623e1e084a6b026f2943
Bryan  Andion <bryan.andion@gmail.com>
2024-07-26 11:58:32 -0500

    cambioss

--- app/Http/Controllers/FuntionController.php
+++ app/Http/Controllers/FuntionController.php
@@ -25,14 +25,43 @@ class FuntionController extends Controller
     {
         $gis = $this->info->DataGisubicacion($id);
 
-        if ($gis) {
+        if (isset($gis) && isset($gis['geometry'])) {
+            // Verificar si la clave 'geometry' existe en el array $gis
+        $latitude = isset($gis['geometry']['latitude']) ? $gis['geometry']['latitude'] : null;
+        $longitude = isset($gis['geometry']['longitude']) ? $gis['geometry']['longitude'] : null;
+
+           if ($latitude && $longitude) {
             $medidor = direcciones::where('contrato', $id)->first();
-            $src =   $gis['geometry']['latitude']   . ',' .  $gis['geometry']['longitude'];
-            return response()->json(['src' => $src, 'gis' => $gis['info'], 'medidor' => $medidor->medidor]); // Si el contrato existe, devuelve sus datos como JSON
+            $src = $latitude . ',' . $longitude;
+            return response()->json([
+                'info'=>'Gis',
+                'src' => $src,
+                'gis' => $gis['info'] ? $gis['info'] : $medidor,
+                'medidor' => $medidor ? $medidor->medidor : 'No disponible'
+            ]);
         } else {
-            return response()->json(['error' => 'Contrato no encontrado'], 404); // Si no existe, devuelve un error
+            return response()->json([
+                'error' => 'Datos de ubicación incompletos',
+                'details' => 'Las coordenadas de latitud o longitud están ausentes.'
+            ], 400); // Código de error 400 para solicitud incorrecta
+        }
+        } else {
+
+            $data = direcciones::where('contrato', $id)->first();
+            $latitude = isset($data->latitud) ? $data->latitud : null;
+            $longitude = isset($data->longitud) ? $data->longitud : null;
+            $src = $latitude . ',' . $longitude;
+
+            return response()->json([
+                'info'=>'Surtigas',
+                'src' => $src,
+                'gis' => $data ? $data : 'No disponible',
+                'medidor' => $data ? $data->medidor : 'No disponible'
+            ]);
         }
     }
+
+
 
     public function exportReports()
     {
