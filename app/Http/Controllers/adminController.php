a32bffce30371bfe5cc77a6ace569f2561b2176c
Skgrendel <bryan.andion@gmail.com>
2024-07-31 06:44:22 -0500

    documentacion controllador admin

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -8,25 +8,36 @@ use Illuminate\Support\Facades\Redirect;
 class adminController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Maneja la solicitud entrante y redirige a diferentes rutas según el rol del usuario.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function __invoke(Request $request)
     {
+        // Verifica si el usuario tiene el rol 'Lector' y redirige a la página de reportes.
         if ($request->user()->hasRole('Lector')) {
             return redirect()->action([ReportesController::class, 'index']);
         }
 
+        // Verifica si el usuario tiene el rol 'Coordinador' y redirige a la página de coordinadores.
         if ($request->user()->hasRole('Coordinador')) {
             return redirect()->action([CoordinadorController::class, 'index']);
         }
+
+        // Verifica si el usuario tiene el rol 'Pno' y redirige a la página de auditorías.
         if ($request->user()->hasRole('Pno')) {
             return redirect()->action([AuditoriaController::class, 'index']);
         }
 
+        // Verifica si el usuario tiene el rol 'Administrador' y redirige a la página de reportes.
         if ($request->user()->hasRole('Administrador')) {
             return redirect()->action([ReportesController::class, 'index']);
         }
 
-        // Redirigir a otra ubicación si el usuario no tiene ninguno de los roles anteriores
+        // Redirigir a otra ubicación si el usuario no tiene ninguno de los roles anteriores.
+        // Puedes definir una ruta de fallback aquí si es necesario.
+        // Ejemplo:
+        // return redirect()->route('fallback.route');
     }
 }
